0b228f2a2a97f17f42fed7ac8c2b04500812c531
BGUstudent <57175258+BGUstudent@users.noreply.github.com>
2019-11-21 12:31:56 +0100

    Add files via upload
    php c'est bien de la merde

--- index.php
+++ index.php
@@ -1,36 +1,17 @@
 <!DOCTYPE html>
 <html lang="en">
 <head>
+    <link rel="stylesheet" href="style.css">
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
 <body>
-<!-- 
-To do:
-définir un cadre/quadrillage
-définir cells et leur état dead or alive
-définir les règles
-    A dead cell with exactly three living neighbours becomes alive.
-    A living cell with two or three living neighbours remains alive.
-    In all other cases, the cell becomes (or remains) dead. 
-définir la "temporalité" n-1
-position aléatoire des cells vivantes
-Next gen / Refresh
-
-Backlog :
-modif vitesse
-modif règles 
-Nombres de cells de départ aléatoires
-Positionnement manuel des cells de départ
-
-Rules:
+<!-- Rules:
 - Une cellule morte possédant exactement trois voisines vivantes devient vivante. 
 - Une cellule vivante possédant deux ou trois voisines vivantes reste vivante. 
-- Sinon elle meurt.
--->
-
+- Sinon elle meurt.-->
 <?php 
 if(isset($_SESSION['world'])){
    session_destroy();
@@ -39,16 +20,23 @@ session_start();
 ?>
 
 <form method="post" action="index.php">
-Taille de la grille: <input type="text" name="size" value="20"><br>
+Taille de la grille: <input type="text" name="size" value="30" size="1" maxlength="2"><br>
 <input type="submit" value="Générer un départ aléatoire" name="submit">
 </form> 
 
-<form method="post" action="index.php">
-<input type="submit" value="next" name="next">
+<form method="post">
+<input type="submit" value="next" name="next" id ="next">
 </form> 
 
-<?php
+<!-- <button type="button" onclick="play()">Play!</button> -->
 
+<!-- <script>
+    function play(){
+        setInterval(function() {document.getElementById("next").click();}, 1000)
+        };
+</script> -->
+
+<?php
 function randomMatrix($size){
     global $world; //je sais que c'est pas recommandé mais fuck j'ai perdu une journée là dessus
     $world=array();
@@ -59,29 +47,33 @@ function randomMatrix($size){
     }
     return $world;
 }
+
 //
 if(isset($_POST['submit']))
 {
     $size = $_POST['size'];
-    global $firstWorld;
-    $firstWorld=randomMatrix($size);
-        echo "<table border='1'>";
+    randomMatrix($size);
+        echo "<table class='center'>";
         for ($x=0; $x<=$size; $x++){
             echo "<tr>";
             for ($y=0; $y<=$size; $y++){
-                echo "<td>";
-                echo $world[$x][$y];
-                echo "</td>";
+                if ($world[$x][$y] == 1){
+                    echo "<td class='alive'></td>";
+                }
+                if ($world[$x][$y] == 0){
+                    echo "<td class='dead'></td>";
+                }
             }
             echo "</tr>";
         }
         echo "</table>";
         $_SESSION['world']=$world;
+        $_SESSION['size']=$size;
     }
 
 // On définit la notion de voisins, on les compte en s'assurant de rester dans les limites de la matrice.
 function voisins($world, $size){
-    echo "<table border='1'>";
+    echo "<table class='center'>";
     for ($x=0; $x <= $size; $x++) {
         echo "<tr>";
         for ($y = 0; $y <= $size; $y++) {
@@ -117,31 +109,26 @@ function voisins($world, $size){
             else if($world[$x][$y]==0 && $liveNeighbours[$x][$y]==3){
                 $new_world[$x][$y]=1;
             }
-            // else if($world[$x][$y]==0 && $liveNeighbours<2){
             else{
                 $new_world[$x][$y]=0;
             }
             //nouvelle matrice
-            echo "<td>";
-            echo $new_world[$x][$y];
-            echo "</td>";
+            if ($new_world[$x][$y] == 1){
+                echo "<td class='alive'></td>";
+            }
+            if ($new_world[$x][$y] == 0){
+                echo "<td class='dead'></td>";
+            }
+            $_SESSION['world']=$new_world;
     }
     echo "</tr>";
 }
 echo "</table>";
 }
-
+// bouton next
 if(isset($_POST['next'])){
- voisins($_SESSION['world'], 20);
+ voisins($_SESSION['world'], $_SESSION['size']);
 }
-// for($x=0;$x<$size;$x++)
-// { 
-//     for($y=0;$y<$size;$y++) 
-//     {
-//         echo $new_world[$x][$y]." ";
-//     }
-//         echo "</p>";
-// }
 
 ?>
 
